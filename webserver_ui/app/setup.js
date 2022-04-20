a592b4fe52d2fb89bd462c1b6942885a6ced9b0a
Marlena Gomez <marlenag@mit.edu>
2022-04-19 20:33:38 -0400

    added tutorial help to start page

--- webserver_ui/app/setup.js
+++ webserver_ui/app/setup.js
@@ -61,19 +61,292 @@ var setupUserInterface = function() {
   mainContext.add(tileTransformModifier).add(tileModifier).add(tile);
 
   startButton = new Surface({
-    content: "Say Start to begin presentation mode",
-    size: [200, 150],
+    content: "Say START to begin presentation mode",
+    size: [250, 150],
     properties: {
       backgroundColor: Colors.GREY,
-      color: "white"
+      color: "black"
     }
   });
   var startModifier = new StateModifier({
     origin: [0.0, 1.0],
     align: [0.0, 0.4],
-    transform: Transform.translate(gridOrigin[0]+300, gridOrigin[1], 0)
+    transform: Transform.translate(gridOrigin[0]+275, gridOrigin[1]-80, 0)
   })
   mainContext.add(startModifier).add(startButton);
+
+  var helpDescription = new Surface({
+    content: "--------------------------------------------------------------------------------------------------------------------------------",
+    size: [700, 10],
+    properties: {
+      backgroundColor: Colors.GREY,
+      color: "black"
+    }
+  });
+  var helpModifier = new StateModifier({
+    origin: [0.0, 1.0],
+    align: [0.0, 0.4],
+    transform: Transform.translate(gridOrigin[0]+50, gridOrigin[1]-160, 0)
+  })
+  mainContext.add(helpModifier).add(helpDescription);
+
+  var commandsSurface = new Surface({
+    content: "Speech and Gesture Commands",
+    size: [210, 150],
+    properties: {
+      backgroundColor: Colors.GREY,
+      color: "black"
+    }
+  });
+  var commandsModifier = new StateModifier({
+    origin: [0.0, 1.0],
+    align: [0.0, 0.4],
+    transform: Transform.translate(gridOrigin[0]+285, gridOrigin[1]-20, 0)
+  })
+  mainContext.add(commandsModifier).add(commandsSurface);
+
+  var speechSurface = new Surface({
+    content: "----SPEECH----",
+     // next slide, next\n previous slide, previous\n laser, start laser, laser pointer\n stop laser, stop\n highlight\n stop highlight, stop\n circle, draw circle",
+    size: [100, 25],
+    properties: {
+      backgroundColor: Colors.GREY,
+      color: "black"
+    }
+  });
+  var speechModifier = new StateModifier({
+    origin: [0.0, 1.0],
+    align: [0.0, 0.4],
+    transform: Transform.translate(gridOrigin[0]+145, gridOrigin[1]-100, 0)
+  })
+  mainContext.add(speechModifier).add(speechSurface);
+
+  var speechSurface = new Surface({
+    content: "----SPEECH----",
+     // next slide, next\n previous slide, previous\n laser, start laser, laser pointer\n stop laser, stop\n highlight\n stop highlight, stop\n circle, draw circle",
+    size: [100, 25],
+    properties: {
+      backgroundColor: Colors.GREY,
+      color: "black"
+    }
+  });
+  var speechModifier = new StateModifier({
+    origin: [0.0, 1.0],
+    align: [0.0, 0.4],
+    transform: Transform.translate(gridOrigin[0]+145, gridOrigin[1]-100, 0)
+  })
+  mainContext.add(speechModifier).add(speechSurface);
+
+  var speechSurface = new Surface({
+    content: "NEXT SLIDE: next, next slide",
+    size: [250, 25],
+    properties: {
+      backgroundColor: Colors.GREY,
+      color: "black"
+    }
+  });
+  var speechModifier = new StateModifier({
+    origin: [0.0, 1.0],
+    align: [0.0, 0.4],
+    transform: Transform.translate(gridOrigin[0]+95, gridOrigin[1]-75, 0)
+  })
+  mainContext.add(speechModifier).add(speechSurface);
+
+  var speechSurface = new Surface({
+    content: "PREVIOUS SLIDE: previous, previous slide",
+    size: [250, 25],
+    properties: {
+      backgroundColor: Colors.GREY,
+      color: "black"
+    }
+  });
+  var speechModifier = new StateModifier({
+    origin: [0.0, 1.0],
+    align: [0.0, 0.4],
+    transform: Transform.translate(gridOrigin[0]+95, gridOrigin[1]-45, 0)
+  })
+  mainContext.add(speechModifier).add(speechSurface);
+
+  var speechSurface = new Surface({
+    content: "LASER POINTER: laser, start laser, laser pointer",
+    size: [250, 25],
+    properties: {
+      backgroundColor: Colors.GREY,
+      color: "black"
+    }
+  });
+  var speechModifier = new StateModifier({
+    origin: [0.0, 1.0],
+    align: [0.0, 0.4],
+    transform: Transform.translate(gridOrigin[0]+95, gridOrigin[1]+5, 0)
+  })
+  mainContext.add(speechModifier).add(speechSurface);
+
+  var speechSurface = new Surface({
+    content: "--uses Leap sensor hand position to control",
+    size: [250, 25],
+    properties: {
+      backgroundColor: Colors.GREY,
+      color: "black"
+    }
+  });
+  var speechModifier = new StateModifier({
+    origin: [0.0, 1.0],
+    align: [0.0, 0.4],
+    transform: Transform.translate(gridOrigin[0]+105, gridOrigin[1]+45, 0)
+  })
+  mainContext.add(speechModifier).add(speechSurface);
+
+  var speechSurface = new Surface({
+    content: "STOP LASER: stop laser, stop",
+    size: [250, 25],
+    properties: {
+      backgroundColor: Colors.GREY,
+      color: "black"
+    }
+  });
+  var speechModifier = new StateModifier({
+    origin: [0.0, 1.0],
+    align: [0.0, 0.4],
+    transform: Transform.translate(gridOrigin[0]+95, gridOrigin[1]+70, 0)
+  })
+  mainContext.add(speechModifier).add(speechSurface);
+
+  var speechSurface = new Surface({
+    content: "HIGHLIGHT: highlight, start highlight",
+    size: [250, 25],
+    properties: {
+      backgroundColor: Colors.GREY,
+      color: "black"
+    }
+  });
+  var speechModifier = new StateModifier({
+    origin: [0.0, 1.0],
+    align: [0.0, 0.4],
+    transform: Transform.translate(gridOrigin[0]+95, gridOrigin[1]+100, 0)
+  })
+  mainContext.add(speechModifier).add(speechSurface);
+
+  var speechSurface = new Surface({
+    content: "--uses hand position at start and end to create highlight box",
+    size: [250, 25],
+    properties: {
+      backgroundColor: Colors.GREY,
+      color: "black"
+    }
+  });
+  var speechModifier = new StateModifier({
+    origin: [0.0, 1.0],
+    align: [0.0, 0.4],
+    transform: Transform.translate(gridOrigin[0]+105, gridOrigin[1]+120, 0)
+  })
+  mainContext.add(speechModifier).add(speechSurface);
+
+  var speechSurface = new Surface({
+    content: "STOP HIGHLIGHT: stop highlight, stop",
+    size: [250, 25],
+    properties: {
+      backgroundColor: Colors.GREY,
+      color: "black"
+    }
+  });
+  var speechModifier = new StateModifier({
+    origin: [0.0, 1.0],
+    align: [0.0, 0.4],
+    transform: Transform.translate(gridOrigin[0]+95, gridOrigin[1]+170, 0)
+  })
+  mainContext.add(speechModifier).add(speechSurface);
+
+  var speechSurface = new Surface({
+    content: "DRAW CIRCLE: circle, draw circle",
+    size: [250, 25],
+    properties: {
+      backgroundColor: Colors.GREY,
+      color: "black"
+    }
+  });
+  var speechModifier = new StateModifier({
+    origin: [0.0, 1.0],
+    align: [0.0, 0.4],
+    transform: Transform.translate(gridOrigin[0]+95, gridOrigin[1]+200, 0)
+  })
+  mainContext.add(speechModifier).add(speechSurface);
+
+  var speechSurface = new Surface({
+    content: "--uses hand position to center circle",
+    size: [250, 25],
+    properties: {
+      backgroundColor: Colors.GREY,
+      color: "black"
+    }
+  });
+  var speechModifier = new StateModifier({
+    origin: [0.0, 1.0],
+    align: [0.0, 0.4],
+    transform: Transform.translate(gridOrigin[0]+105, gridOrigin[1]+220, 0)
+  })
+  mainContext.add(speechModifier).add(speechSurface);
+
+
+  var gestureSurface = new Surface({
+    content: "---GESTURE---",
+    size: [100, 25],
+    properties: {
+      backgroundColor: Colors.GREY,
+      color: "black"
+    }
+  });
+  var gestureModifier = new StateModifier({
+    origin: [0.0, 1.0],
+    align: [0.0, 0.4],
+    transform: Transform.translate(gridOrigin[0]+545, gridOrigin[1]-100, 0)
+  })
+  mainContext.add(gestureModifier).add(gestureSurface);
+
+  var gestureSurface = new Surface({
+    content: "NEXT SLIDE: hand swipe left or up",
+    size: [225, 25],
+    properties: {
+      backgroundColor: Colors.GREY,
+      color: "black"
+    }
+  });
+  var gestureModifier = new StateModifier({
+    origin: [0.0, 1.0],
+    align: [0.0, 0.4],
+    transform: Transform.translate(gridOrigin[0]+495, gridOrigin[1]-70, 0)
+  })
+  mainContext.add(gestureModifier).add(gestureSurface);
+
+  var gestureSurface = new Surface({
+    content: "PREVIOUS SLIDE: hand swipe right or down",
+    size: [200, 25],
+    properties: {
+      backgroundColor: Colors.GREY,
+      color: "black"
+    }
+  });
+  var gestureModifier = new StateModifier({
+    origin: [0.0, 1.0],
+    align: [0.0, 0.4],
+    transform: Transform.translate(gridOrigin[0]+495, gridOrigin[1]-40, 0)
+  })
+  mainContext.add(gestureModifier).add(gestureSurface);
+
+  var gestureSurface = new Surface({
+    content: "DRAW CIRCLE: pointer finger circle gesture",
+    size: [250, 25],
+    properties: {
+      backgroundColor: Colors.GREY,
+      color: "black"
+    }
+  });
+  var gestureModifier = new StateModifier({
+    origin: [0.0, 1.0],
+    align: [0.0, 0.4],
+    transform: Transform.translate(gridOrigin[0]+495, gridOrigin[1]+10, 0)
+  })
+  mainContext.add(gestureModifier).add(gestureSurface);
 
   // Draw the cursor
   cursorSurface = new Surface({

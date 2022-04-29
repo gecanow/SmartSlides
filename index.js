5646f8f925563f4ce8638cc3cbb0de6accbe25fb
Gabrielle Ecanow <gecanow@mit.edu>
2022-04-29 10:07:28 -0400

    FINALLY working, good stopping point

--- index.js
+++ index.js
@@ -2,29 +2,33 @@
  * ..... Express Webapp .....
  */
 
- const express = require('express');
- const path = require('path');
- const fileURLToPath = require('url').fileURLToPath;
- 
- __filename = fileURLToPath(require('url').pathToFileURL(__filename).toString());
- __dirname = path.dirname(__filename);
- 
- const app = express();
- const port = process.env.PORT || "8000";  // TODO: will likely not be used
- 
- app.use(express.static(path.join(__dirname, "public/presentation")));
- app.use((request, response, next) => {
-   // allow requests from web pages hosted anywhere
-   response.set('Access-Control-Allow-Origin', '*');
-   next();
- });
- 
- app.get('/', (req, res) => {
-   const file = path.join(__dirname, 'public', 'presentation', 'index.html');
-   console.log("serving", file);
-   res.sendFile(file);
- });
- 
- app.listen(port, () => {
-   console.log(`Listening to requests on http://localhost:${port}`);
- });
+const express = require('express');
+const path = require('path');
+const app = express();
+const port = 8000;
+
+// main prezzy
+app.use(express.static(path.join(__dirname, 'public/presentation')));
+
+// where to find node_mods
+app.use('/scripts', express.static(path.join(__dirname, 'node_modules')));
+
+// src
+app.use('/src', express.static(path.join(__dirname, 'src')));
+
+
+app.use((request, response, next) => {
+  // allow requests from web pages hosted anywhere
+  response.set('Access-Control-Allow-Origin', '*');
+  next();
+});
+
+app.get('/', function(req, res) {
+  const file = path.join(__dirname, 'public', 'presentation', 'index.html');
+  console.log("serving", file);
+  res.sendFile(file);
+});
+
+app.listen(port, function() {
+  console.log(`Listening to requests on http://localhost:${port}`)
+});
---
+++ src/presentation-control.js
@@ -0,0 +1,152 @@
+export function setup() {
+    console.log("...setting up...");
+
+    // configure the head
+    const head = document.head;
+    head.title = "SmartSlides";
+    const link = document.createElement("link");
+    link.type = "text/css";
+    link.rel = "stylesheet";
+    link.href = "https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css";
+    head.appendChild(link);
+    
+    // add the jumbotron
+    const jumboDiv = document.createElement("div");
+    jumboDiv.className = "jumbotron";
+    jumboDiv.style.display = "flex";
+    jumboDiv.style.justifyContent = "center";
+    jumboDiv.innerHTML = `
+        <div style="flex-direction:column; align-items: center;">
+            <h1>SmartSlides</h1>
+            <input id="slideX"></input>
+            <button id="appleBttn">Run Apple Script</button>
+        </div>
+    `;
+    document.body.insertBefore(jumboDiv, document.body.firstChild);
+
+    // restyle the stagingArea
+    const stage = document.getElementById("stageArea");
+    if (stage) {
+        stage.style.display = 'none';
+        stage.style.marginTop = "100px";
+        stage.style.display = 'block';
+    }
+
+    // finally, connect the script!
+    document.addEventListener('keydown', function (e) {
+        // console.log(e);
+        // console.log(gShowController);
+
+        if (e.key === 'r') {
+            prev();
+        }
+        else if (e.key === 't') {
+            next();
+        }
+        else if (e.key === 'y') {
+            const value = parseInt(document.getElementById("slideX")?.nodeValue ?? gShowController.currentSlideIndex);
+            jumpSlide(value);
+        }
+        else if (e.key === 'u') {
+            let value = parseInt(document.getElementById("slideX")?.nodeValue ?? gShowController.currentSlideIndex);
+            jumpScene(value);
+        }
+
+    });
+
+    document.getElementById("appleBttn")?.addEventListener("onclick", function(e) {
+        convertToHTML();
+    });
+}
+
+// Programmatically move to the prev slide
+export function prev() {
+    console.log("prev");
+    let event = new KeyboardEvent('keydown', {'charCode': 37 /*kKeyCode_LeftArrow*/});
+    gShowController.handleKeyDownEvent(event);
+}
+
+// Programmatically move to the next slide
+export function next() {
+    console.log("next");
+    let event = new KeyboardEvent('keydown', {'charCode': 39 /*kKeyCode_RightArrow*/});
+    gShowController.handleKeyDownEvent(event);
+}
+
+// Programmatically move to slide X
+export function jumpSlide(x) {
+    console.log("jumpSlide");
+    gShowController.jumpToSlide(x);
+}
+
+// Programmatically move to scene X
+export function jumpScene(x) {
+    console.log("jumpScene");
+    gShowController.jumpToScene(x);
+}
+
+// Programmatically rewinds X slides back
+export function rewindSlide(x) {
+    console.log("rewindSlide");
+    jumpSlide(gShowController, gShowController.currentSlideIndex - x);
+}
+
+// Programmatically rewinds X scenes back
+export function rewindScene(x) {
+    console.log("rewindScene");
+    jumpScene(gShowController, gShowController.currentSlideIndex - x);
+}
+
+// Programmatically fast-forwards X slides back
+export function ffSlide(x) {
+    console.log("ffSlide");
+    jumpSlide(gShowController, gShowController.currentSlideIndex + x);
+}
+
+// Programmatically fast-forwards  X scenes back
+export function ffScene(x) {
+    console.log("ffScene");
+    jumpScene(gShowController, gShowController.currentSlideIndex + x);
+}
+
+export async function convertToHTML() {
+    // Very basic AppleScript command. Returns the song name of each
+    // currently selected track in iTunes as an 'Array' of 'String's.
+    const script = `
+    -- https://iworkautomation.com/keynote/document-export.html
+    tell application "Keynote"
+        activate
+        
+        set tempDir to "tmp"
+        
+        -- destination folder
+        tell application "Finder"
+            set currentFolder to container of (path to me) as alias
+            set the chosenDocumentFile to (choose file default location currentFolder with prompt "Choose the presentation:")
+            set the targetFolderHFSPath to (currentFolder & tempDir) as string
+        end tell
+        
+        --export doc
+        set the chosenDocument to (open chosenDocumentFile)
+        with timeout of 1200 seconds
+            export chosenDocument as HTML to file targetFolderHFSPath
+        end timeout
+        close chosenDocument
+        
+        -- move the new assets into the public/presentation folder
+        tell application "Finder"
+            activate
+            set assetsFolder to POSIX path of (currentFolder as string) & (tempDir as string) & ("/assets" as string)
+            set destFolder to POSIX path of (currentFolder as string) & ("../../../public/presentation/assets" as string)
+            move folder assetsFolder to folder destFolder
+        end tell
+        
+        display dialog "Done"
+    end tell
+    `;
+
+    // console.log(`attempting to run ${script}`);
+    // const runAppleScript = require('run-applescript');
+    // const result = await runAppleScript(script);
+    // console.log(result);
+}

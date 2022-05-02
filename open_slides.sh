c5817bd522e66dce23094a215afda6bfd18d50b9
Gabrielle Ecanow <gecanow@mit.edu>
2022-05-02 10:33:33 -0400

    cleaned up bash script and gitignore

--- open_slides.sh
+++ open_slides.sh
@@ -1,5 +1,4 @@
 #!/bin/sh
 
 cd "$(dirname "$0")"                        # https://stackoverflow.com/questions/3349105/how-can-i-set-the-current-working-directory-to-the-directory-of-the-script-in-ba
-npm start                                   # start!
-open http://localhost:8000                  # and open the localhost!
+npm start & open http://localhost:8000      # start!

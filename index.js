f6a1eebbfd73f89ad7f11a9e2ba7d8c9048ffec3
Gabrielle Ecanow <gecanow@mit.edu>
2022-05-03 14:15:10 -0400

    WIP modal stuff

--- index.js
+++ index.js
@@ -15,7 +15,7 @@ app.use(express.static(path.join(__dirname, 'public/presentation')));
 // app.use(express.static(path.join(__dirname, 'public')));
 
 // where to find node_mods
-app.use('/scripts', express.static(path.join(__dirname, 'node_modules')));
+app.use('/nm', express.static(path.join(__dirname, 'node_modules')));
 
 // src
 app.use('/src', express.static(path.join(__dirname, 'src')));

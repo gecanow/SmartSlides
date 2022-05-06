8171a7823509ef5a7184dbd8144ddfcd4a90acf6
Gabrielle Ecanow <gecanow@mit.edu>
2022-05-06 11:39:58 -0400

    got rid of unused helper

--- public/presentation/app/helpers.js
+++ public/presentation/app/helpers.js
@@ -18,23 +18,3 @@ var generateSpeech = function(message, callback) {
     speechSynthesis.speak(msg);
   }
 };
-//
-// getIntersectingTile(screenPosition)
-//    Returns the tile enclosing the input screen position
-// Input:
-//    screenPosition = [x,y]
-// Output:
-//    tilePosition = {row: r, col: c}, if intersecting the board
-//    false, if not intersecting the board
-var getIntersectingTile = function(screenPosition) {
-  if (screenPosition[0] >= gridOrigin[0] && screenPosition[0] <= gridOrigin[0] + BOARDSIZE
-    && screenPosition[1] >= gridOrigin[1] && screenPosition[1] <= gridOrigin[1] + BOARDSIZE) {
-    var column = Math.floor((screenPosition[0] - gridOrigin[0]) / TILESIZE);
-    var row = Math.floor((screenPosition[1] - gridOrigin[1]) / TILESIZE);
-    var tile = tiles[row*NUMTILES + column];
-    return {row: row, col: column};
-  }
-  else {
-    return false;
-  }
-};
